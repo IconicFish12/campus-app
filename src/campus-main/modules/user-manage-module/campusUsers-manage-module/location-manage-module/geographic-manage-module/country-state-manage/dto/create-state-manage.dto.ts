f199c585b97d732564961318214b48bf5ad708e5
Ibnu Syawal Aliefian <superglidingogre0571@gmail.com>
2025-07-20 23:30:48 +0700

    [ADD]: Add CRUD process in country state manage mopdule

--- src/campus-main/modules/user-manage-module/campusUsers-manage-module/location-manage-module/geographic-manage-module/country-state-manage/dto/create-state-manage.dto.ts
+++ src/campus-main/modules/user-manage-module/campusUsers-manage-module/location-manage-module/geographic-manage-module/country-state-manage/dto/create-state-manage.dto.ts
@@ -1,1 +1,25 @@
-export class CreateStateManageDto {}
+import { IsNotEmpty, IsString, IsUUID, MinLength } from 'class-validator';
+
+export class CreateStateManageDto {
+  @IsString({
+    message: 'Country id must be a text',
+  })
+  @IsUUID('4', {
+    message: 'Country id must be a uuid version 4',
+  })
+  @IsNotEmpty({
+    message: 'country id cannot be empty',
+  })
+  readonly countryId: string;
+
+  @IsString({
+    message: 'Country State name must be a text',
+  })
+  @MinLength(4, {
+    message: 'Country State name must grater than 4 character',
+  })
+  @IsNotEmpty({
+    message: 'Country State name cannot be empty',
+  })
+  readonly name: string;
+}
--- src/campus-main/modules/user-manage-module/campusUsers-manage-module/location-manage-module/geographic-manage-module/country-state-manage/state-manage.service.ts
+++ src/campus-main/modules/user-manage-module/campusUsers-manage-module/location-manage-module/geographic-manage-module/country-state-manage/state-manage.service.ts
@@ -1,26 +1,133 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateStateManageDto } from './dto/create-state-manage.dto';
 import { UpdateStateManageDto } from './dto/update-state-manage.dto';
+import { CampusDbService } from 'src/common/Database/campus-db/campus-db.service';
 
 @Injectable()
 export class StateManageService {
-  create(createStateManageDto: CreateStateManageDto) {
-    return 'This action adds a new countryStateManage';
+  constructor(private readonly prisma: CampusDbService) {}
+  async create(createRequest: CreateStateManageDto) {
+    try {
+      const countryExits = await this.prisma.country.findUnique({
+        where: {
+          id: createRequest.countryId,
+        },
+      });
+
+      if (!countryExits) {
+        throw new BadRequestException(
+          `Country Data with id ${createRequest.countryId} is not found`,
+        );
+      }
+
+      return await this.prisma.state.create({
+        data: {
+          name: createRequest.name,
+          country: {
+            connect: {
+              id: createRequest.countryId,
+            },
+          },
+        },
+      });
+    } catch {
+      throw new InternalServerErrorException(
+        'Error Acquired While Creating Country Data',
+      );
+    }
   }
 
-  findAll() {
-    return `This action returns all countryStateManage`;
+  async findAll() {
+    try {
+      return await this.prisma.state.findMany({
+        include: {
+          country: true,
+          _count: {
+            select: {
+              cities: true,
+            },
+          },
+        },
+        orderBy: {
+          createdAt: 'asc',
+        },
+      });
+    } catch {
+      throw new InternalServerErrorException(
+        'Error Acquired While Getting Country Data',
+      );
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} countryStateManage`;
+  async findOne(id: string) {
+    try {
+      const state = await this.prisma.state.findUnique({
+        where: { id: id },
+        include: {
+          country: { select: { id: true, name: true } },
+          _count: { select: { cities: true } },
+        },
+      });
+      if (!state) {
+        throw new NotFoundException(
+          `Province or country state with ID "${id}" is not found`,
+        );
+      }
+      return state;
+    } catch {
+      throw new InternalServerErrorException(
+        'Error Acquired While Getting Country Data',
+      );
+    }
   }
 
-  update(id: number, updateCountryStateManageDto: UpdateStateManageDto) {
-    return `This action updates a #${id} countryStateManage`;
+  async update(id: string, updateRequest: UpdateStateManageDto) {
+    try {
+      await this.findOne(id);
+
+      if (updateRequest.countryId) {
+        const countryExists = await this.prisma.country.findUnique({
+          where: { id: updateRequest.countryId },
+        });
+        if (!countryExists) {
+          throw new BadRequestException(
+            `country with ID "${updateRequest.countryId}" is not found`,
+          );
+        }
+      }
+
+      return await this.prisma.state.update({
+        where: { id: id },
+        data: {
+          name: updateRequest.name,
+          country: updateRequest.countryId
+            ? { connect: { id: updateRequest.countryId } }
+            : undefined,
+          updatedAt: new Date(),
+        },
+      });
+    } catch {
+      throw new InternalServerErrorException(
+        'Error Acquired While updating Country Data',
+      );
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} countryStateManage`;
+  async remove(id: string) {
+    try {
+      await this.findOne(id);
+      return await this.prisma.state.delete({
+        where: { id: id },
+      });
+    } catch {
+      throw new InternalServerErrorException(
+        'Error Acquired While Deleting Country Data',
+      );
+    }
   }
 }
