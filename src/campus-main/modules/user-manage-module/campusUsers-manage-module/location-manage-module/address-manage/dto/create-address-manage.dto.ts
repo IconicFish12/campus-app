d777f3374aca2581da1522a5bd329e95eb4146be
Ibnu Syawal Aliefian <superglidingogre0571@gmail.com>
2025-07-21 18:53:44 +0700

    [ADD] : Add CRUD process in address manage module

--- src/campus-main/modules/user-manage-module/campusUsers-manage-module/location-manage-module/address-manage/dto/create-address-manage.dto.ts
+++ src/campus-main/modules/user-manage-module/campusUsers-manage-module/location-manage-module/address-manage/dto/create-address-manage.dto.ts
@@ -1,1 +1,55 @@
-export class CreateAddressManageDto {}
+import {
+  IsInt,
+  IsNotEmpty,
+  IsString,
+  IsUUID,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateAddressManageDto {
+  @IsString({
+    message: 'Address is must be a text',
+  })
+  @MinLength(4, {
+    message: 'Address must grater than 10 character',
+  })
+  @MaxLength(50, {
+    message: 'Address must less than 50 character',
+  })
+  @IsNotEmpty({
+    message: 'Address is cannot be empty',
+  })
+  readonly fullAddress: string;
+
+  @IsString({
+    message: 'User id must be a text',
+  })
+  @IsUUID('4', {
+    message: 'User id must be a uuid version 4',
+  })
+  @IsNotEmpty({
+    message: 'User id cannot be empty',
+  })
+  readonly userId: string;
+
+  @IsString({
+    message: 'City id must be a text',
+  })
+  @IsUUID('4', {
+    message: 'City id must be a uuid version 4',
+  })
+  @IsNotEmpty({
+    message: 'City id cannot be empty',
+  })
+  readonly cityId: string;
+
+  @IsInt({
+    message: 'Postal Code is must be a number',
+  })
+  @IsNotEmpty({
+    message: 'Postal Code is cannot be empty',
+  })
+  //   @IsPostalCode()
+  readonly postalCode: number;
+}
