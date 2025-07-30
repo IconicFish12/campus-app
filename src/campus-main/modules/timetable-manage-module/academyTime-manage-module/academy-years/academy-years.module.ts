054da087412f09dd761761d8c6c211dd28403349
Ibnu Syawal Aliefian <superglidingogre0571@gmail.com>
2025-07-30 23:17:39 +0700

    [MODIF]: Initiating Campus DB Module in all management Module in Campus Main Service

--- src/campus-main/modules/timetable-manage-module/academyTime-manage-module/academy-years/academy-years.module.ts
+++ src/campus-main/modules/timetable-manage-module/academyTime-manage-module/academy-years/academy-years.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AcademyYearsService } from './academy-years.service';
 import { AcademyYearsController } from './academy-years.controller';
+import { CampusDbService } from 'src/common/Database/campus-db/campus-db.service';
 
 @Module({
   controllers: [AcademyYearsController],
-  providers: [AcademyYearsService],
+  providers: [AcademyYearsService, CampusDbService],
   exports: [AcademyYearsService],
 })
 export class AcademyYearsModule {}
--- src/campus-main/modules/timetable-manage-module/schedule-manage-module/lecturers-schedule/lectures-schedule.module.ts
+++ src/campus-main/modules/timetable-manage-module/schedule-manage-module/lecturers-schedule/lectures-schedule.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LecturesScheduleService } from './lectures-schedule.service';
 import { LecturesScheduleController } from './lectures-schedule.controller';
+import { CampusDbService } from 'src/common/Database/campus-db/campus-db.service';
 
 @Module({
   controllers: [LecturesScheduleController],
-  providers: [LecturesScheduleService],
+  providers: [LecturesScheduleService, CampusDbService],
+  exports: [LecturesScheduleService],
 })
 export class LecturesScheduleModule {}
