b2035fea3db5ba38c4bc576dc6a3b94a68e5a8c2
Ibnu Syawal Aliefian <superglidingogre0571@gmail.com>
2025-11-24 17:03:06 +0700

    [CHANGE]: change database configuration for each service

---
+++ src/services/campus-lms/src/database/database.service.ts
@@ -0,0 +1,25 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from './generated/lms-client/client.mjs';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { env } from 'prisma/config';
+
+@Injectable()
+export class DatabaseService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: env('DATABASE_URL'),
+    });
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
